8e04b3a01dfb2958e65fb0dc1a11a40a6c6126d4
UnknownFur <123666616+UnknownFur@users.noreply.github.com>
2023-04-13 16:01:32 -0500

    Add files via upload

---
+++ ej01/cubes.js
@@ -0,0 +1,92 @@
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColor(0xDDDDDD, 1);
+document.body.appendChild(renderer.domElement);
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(80, WIDTH / HEIGHT);
+camera.position.z = 4.5;
+camera.position.x = -1.2;
+camera.position.y = 2;
+
+camera.rotation.set(0, -0.5, 0);
+
+scene.add(camera);
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+// Tamaño Cubo Inicial
+Size = 2;
+// Cantidad Cubos
+Cubes = 0
+// Posición cubos
+x = Size / 2;
+y = Size / 2;
+z = Size / 2;
+
+c = 0
+cube = [];
+material = [];
+color = [];
+
+function animation1(){
+  color[c] = Math.random() * 0xffffff;
+  var geometry = new THREE.BoxGeometry(Size, Size, Size);
+  material.push(new THREE.MeshPhongMaterial({ color: color[c] }));
+  cube[c] = new THREE.Mesh(geometry, material[c]);
+
+  cube[c].translateX(x);
+  cube[c].translateY(y);
+  cube[c].translateZ(z);
+  
+  y = y + (Size - (Size/4));
+  
+  Size = Size / 2;
+  scene.add(cube[c])
+}
+
+function animation2(){
+    cube[c].position.x += 5;
+}
+
+const light1 = new THREE.DirectionalLight(0x00ffff, 1);
+light1.position.set(-1, 2, 4);
+scene.add(light1);
+
+const light2 = new THREE.DirectionalLight(0x00ffff, 1);
+light2.position.set(1, -2, -4);
+scene.add(light2);
+
+const size = 150;
+const divisions = 160;
+const axesHelper = new THREE.AxesHelper(1000);
+scene.add(axesHelper);
+
+s = 0;
+const gridHelper = new THREE.GridHelper(size, divisions);
+scene.add(gridHelper);
+document.addEventListener('keydown', function(event) {
+  switch(event.keyCode) {
+      case 83:
+      if (s == 0){
+        animation1();
+        s = 1;
+      }
+      if (s == 1){
+        animation2();
+        c++;
+        s = 0;
+      }
+      break;
+  }
+});
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+render();
